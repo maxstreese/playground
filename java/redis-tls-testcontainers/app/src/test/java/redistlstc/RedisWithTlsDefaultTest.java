b1bbba18a9294fdcbc13535137f9809c477ae276
Max Streese <max@streese.com>
2023-05-05 12:15:29 +0200

    Improve the redis with tls test

--- java/redis-tls-testcontainers/app/src/test/java/redistlstc/RedisWithTlsDefaultTest.java
+++ java/redis-tls-testcontainers/app/src/test/java/redistlstc/RedisWithTlsDefaultTest.java
@@ -1,7 +1,6 @@
 package redistlstc;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
@@ -11,22 +10,16 @@ import redis.clients.jedis.Jedis;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.security.*;
 import java.security.cert.CertificateException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// @Disabled
 @Testcontainers
 class RedisWithTlsDefaultTest {
 
     private static final int PORT = 6379;
-
-    @TempDir
-    private Path tmpDir;
 
     @Container
     public GenericContainer redis =
@@ -44,22 +37,22 @@ class RedisWithTlsDefaultTest {
                             "--tls-ca-cert-file", "/tests/tls/ca.crt");
 
     @Test
-    public void testSetAndGet() throws IOException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException, KeyManagementException {
-
-        var redisCertFile = tmpDir.resolve("redis.p12");
-        var caCertFile = tmpDir.resolve("ca.p12");
-
-        redis.copyFileFromContainer("/tests/tls/redis.p12", redisCertFile.toString());
-        redis.copyFileFromContainer("/tests/tls/ca.p12", caCertFile.toString());
+    public void testSetAndGet() throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException, KeyManagementException {
 
         var keyStore = KeyStore.getInstance("pkcs12");
-        keyStore.load(new FileInputStream(redisCertFile.toString()), "secret".toCharArray());
+        redis.copyFileFromContainer("/tests/tls/redis.p12", inputStream -> {
+            keyStore.load(inputStream, "secret".toCharArray());
+            return null;
+        });
 
         var keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         keyManagerFactory.init(keyStore, "secret".toCharArray());
 
         var trustStore = KeyStore.getInstance("pkcs12");
-        trustStore.load(new FileInputStream(caCertFile.toString()), "secret".toCharArray());
+        redis.copyFileFromContainer("/tests/tls/ca.p12", inputStream -> {
+            trustStore.load(inputStream, "secret".toCharArray());
+            return null;
+        });
 
         var trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustManagerFactory.init(trustStore);
@@ -67,9 +60,10 @@ class RedisWithTlsDefaultTest {
         var sslContext = SSLContext.getInstance("TLS");
         sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
 
-        var jedis = new Jedis(redis.getHost(), redis.getMappedPort(PORT), true, sslContext.getSocketFactory(), sslContext.getDefaultSSLParameters(), null);
-        assertThat(jedis.hset("foo", "bar", "baz")).isEqualTo(1);
-        assertThat(jedis.hget("foo", "bar")).isEqualTo("baz");
+        try (Jedis jedis = new Jedis(redis.getHost(), redis.getMappedPort(PORT), true, sslContext.getSocketFactory(), sslContext.getDefaultSSLParameters(), null)) {
+            assertThat(jedis.hset("foo", "bar", "baz")).isEqualTo(1);
+            assertThat(jedis.hget("foo", "bar")).isEqualTo("baz");
+        }
 
     }
 
