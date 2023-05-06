0ef179498c28fe8a7318762ad6e7ba31ed63e92d
Max Streese <max@streese.com>
2023-05-06 12:39:54 +0200

    Improve (I think) the Redis without TLS test

--- java/redis-tls-testcontainers/app/src/test/java/redistlstc/RedisWithoutTlsTest.java
+++ java/redis-tls-testcontainers/app/src/test/java/redistlstc/RedisWithoutTlsTest.java
@@ -1,7 +1,5 @@
 package redistlstc;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -11,30 +9,21 @@ import redis.clients.jedis.Jedis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled
 @Testcontainers
 class RedisWithoutTlsTest {
 
     private static final int PORT = 6379;
 
-    private Jedis jedis;
-
     @Container
     public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:7"))
             .withExposedPorts(PORT);
 
-    @BeforeEach
-    public void setUp() {
-        var address = redis.getHost();
-        var port    = redis.getMappedPort(PORT);
-        jedis = new Jedis(address, port);
-    }
-
     @Test
     public void testSetAndGet() {
-        jedis.hset("test", "foo", "bar");
-        jedis.hget("test", "foo");
-        assertThat(jedis.hget("test", "foo")).isEqualTo("bar");
+        try (Jedis jedis = new Jedis(redis.getHost(), redis.getMappedPort(PORT))) {
+            assertThat(jedis.hset("foo", "bar", "baz")).isEqualTo(1);
+            assertThat(jedis.hget("foo", "bar")).isEqualTo("baz");
+        }
     }
 
 }
